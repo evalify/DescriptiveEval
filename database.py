bddefb4e8f45bd2e324ff182a23d1a4745ccf316
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2025-01-20 13:21:20 +0530

    Enhance type hinting for database connection functions in database.py

--- database.py
+++ database.py
@@ -3,13 +3,19 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from pymongo import MongoClient
 from dotenv import load_dotenv
+# For Type Hinting
+from typing import Tuple
+from pymongo.database import Database
+from psycopg2.extensions import connection as postgres_connection
 
 load_dotenv()
 
-def get_postgres_cursor():
-    connection = psycopg2.connect(os.getenv('COCKROACH_DB'))
-    return connection.cursor(cursor_factory=RealDictCursor), connection
 
-def get_mongo_client():
+def get_postgres_cursor() -> Tuple[RealDictCursor, postgres_connection]:
+    my_connection = psycopg2.connect(os.getenv('COCKROACH_DB'))
+    return my_connection.cursor(cursor_factory=RealDictCursor), my_connection
+
+
+def get_mongo_client() -> Database:
     client = MongoClient(os.getenv("MONGODB_URI"))
     return client["Evalify"]
